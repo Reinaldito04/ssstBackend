9128bc6ed408702dd087b3e18355aa1122404c8e
reinaldito <reybellorin82@gmail.com>
2024-10-11 14:25:36 -0400

    "Added new models and API endpoints for Desviaciones and SeguimientoDesviaciones, updated database queries and error handling."

--- models/Desviaciones.py
+++ models/Desviaciones.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-
+from typing import Optional
 class Desviaciones(BaseModel):
     Descripcion: str
     Area : str 
@@ -10,3 +10,19 @@ class Desviaciones(BaseModel):
     Nivel: str 
     AccionesCorrectivas : str 
     Deteccion : str
+    
+class DesviacionesResponse(Desviaciones):
+    ID: int
+    
+class SeguimientoDesviaciones(BaseModel):
+    IDDesviacion: int 
+    Deteccion: Optional[str]
+    Seguimiento: Optional[str]
+    Avance: Optional[str]
+    Responsable: Optional[str]
+    Observacion: Optional[str]
+    
+class SeguimientoDesviacionResponse(SeguimientoDesviaciones):
+    ID: Optional[int]  # El campo ID ahora es opcional
+    Descripcion: Optional[str]
+    Area: Optional[str]
--- routers/Desviaciones.py
+++ routers/Desviaciones.py
@@ -1,11 +1,84 @@
-from fastapi import APIRouter,HTTPException
-from models.Desviaciones import Desviaciones
+from fastapi import APIRouter, HTTPException
+from models.Desviaciones import Desviaciones, DesviacionesResponse, SeguimientoDesviacionResponse
 from db.db import get_db
 
 router = APIRouter(
     prefix="/desviaciones",
     tags=["Desviaciones"],
 )
+
+
+@router.get('/getSeguimiento')
+def getSeguimiento():
+    try:
+        with get_db() as connect:
+            cursor = connect.cursor()
+            cursor.execute("""     
+                           SELECT 
+                            d.ID AS ID,
+                            d.Descripcion AS Descripcion,
+                            d.Area AS Area,
+                            s.Deteccion AS Deteccion,
+                            s.Seguimiento AS Seguimiento,
+                            s.Avance,
+                            s.Responsable,
+                            s.Observacion,
+                            s.ID
+                        FROM 
+                            Desviaciones d
+                        LEFT JOIN 
+                            SeguimientoDesviacion s
+                        ON 
+                            d.ID = s.IDDesviacion;
+                           """)
+            result = cursor.fetchall()
+            seguimiento = []
+            for row in result:
+                 seguimiento.append(SeguimientoDesviacionResponse(
+                    IDDesviacion=row[0],
+                    Descripcion=row[1],
+                    Area=row[2],
+                    Deteccion=row[3],
+                    Seguimiento=row[4],
+                    Avance=row[5] if row[5] is not None else None,
+                    Responsable=row[6] if row[6] is not None else None,
+                    Observacion=row[7] if row[7] is not None else None,
+                    ID=row[8] if row[8] is not None else None  # Manejar NULL en el campo ID
+                ))
+            return seguimiento
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error al obtener seguimiento: {str(e)}")
+
+
+@router.get('/getDesviaciones')
+def getDesviaciones():
+    try:
+        with get_db() as connect:
+            cursor = connect.cursor()
+            cursor.execute(
+                'SELECT ID, Descripcion, Area, CausaRaiz, TipoInspeccion, Severidad, Frecuencia, Nivel, AccionesCorrectivas, Deteccion FROM Desviaciones')
+            result = cursor.fetchall()
+            desviaciones = []
+            for row in result:
+                desviaciones.append(DesviacionesResponse(
+                    ID=row[0],
+                    Descripcion=row[1],
+                    Area=row[2],
+                    CausaRaiz=row[3],
+                    TipoInspeccion=row[4],
+                    Severidad=row[5],
+                    Frecuencia=row[6],
+                    Nivel=row[7],
+                    AccionesCorrectivas=row[8],
+                    Deteccion=row[9]
+                ))
+            return desviaciones
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error al obtener desviaciones: {str(e)}")
+
+
 @router.post('/addDesviacion')
 def addDesviacion(desviacion: Desviaciones):
     try:
@@ -27,5 +100,3 @@ def addDesviacion(desviacion: Desviaciones):
         # Manejar cualquier error que ocurra
         raise HTTPException(
             status_code=500, detail=f"Error al agregar desviacion: {str(e)}")
-        
-
